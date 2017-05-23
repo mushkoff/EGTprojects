7af9df294f3b8a45be839842e85e8d56d6f6b482
user-PC\user <michaelmushkoff@gmail.com>
2017-05-23 13:06:48 +0300

    Added swtich case in main

--- Libraries/Format currency/LibFunctions.cpp
+++ Libraries/Format currency/LibFunctions.cpp
@@ -14,12 +14,32 @@ using namespace std;
 int main() {
 
 	double a;
-	string str;
+	char USD = '\u0024';
+	string GBR = "£ GBR "; // char symbols are not working for GBR char(156)
+	string EUR = "EUR "; // char symbols are not working for EUR
+	int str;
 	cout << "Please enter the amount of your money: " << endl;
 	cin >> a;
-	cout << "Please enter your currency in symbol (USD,EU,GBR.etc): " << endl;
+	cout << "Please enter your currency in symbols (USD,EU,GBR.etc)(: " << endl;
+	cout << "For USD please press 1" << endl;
+	cout << "For GBR please press 2" << endl;
+	cout << "For EUR please press 3" << endl;
 	cin >> str;
-	cout << currencyFormat(a) << str << endl;
+
+	switch (str) {
+	case 1:
+		cout << USD;
+		break;
+	case 2:
+		cout << GBR;
+		break;
+	case 3:
+		cout << EUR;
+		break;
+	default:
+		cout << "Invalid choice" << endl;
+	}
+	cout << currencyFormat(a) << endl;
 
 	return 0;
 }
